82834d69d7de4ad1d42bc6c197c8c83a46cadd29
mohammad <mohammadtbz76@gmail.com>
2024-05-18 00:22:29 +0330

    check

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Log;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -16,6 +17,8 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        Log::info('done');
+
         return [
             "name"          =>  $this->faker->title ,
             "slug"          =>  $this->faker->slug ,
